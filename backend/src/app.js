bf3c66890951840d3cd5e8ee539282c9a2152657
Aman <amanyadav923949@gmail.com>
2025-11-07 21:15:35 +0530

    enhance CORS configuration to support dynamic allowed origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,32 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ApiError } from "./utils/apiError.js";
+import "./config.js";
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:5173"], credentials: true}));
+
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS
+  ? process.env.CORS_ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
+  : [];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+    
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        console.log(`[CORS] Allowed origin: ${origin}`);
+        return callback(null, true);
+      } else {
+        console.warn(`[CORS] Blocked origin: ${origin}`);
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit: "16kb"}));
 
